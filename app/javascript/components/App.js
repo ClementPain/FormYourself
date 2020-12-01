31c912abe94cdf2762e435349e7d67fc63e9abf0
clement <clementpain@outlook.com>
2020-12-01 18:06:19 +0100

    first commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World ! Ca va ?
+      </div>
+    )
+  }
+}
+
+export default App
